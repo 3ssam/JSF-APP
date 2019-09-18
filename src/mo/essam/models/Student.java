a461435d79f2e16beed9f500fb05db9b0ef0a5b7
Mohamed Essam <ana3ssam2017@gmail.com>
2019-09-18 12:02:43 +0200

    Use Two DropList one get its items from manage beans and another from its page.

--- src/mo/essam/models/Student.java
+++ src/mo/essam/models/Student.java
@@ -1,5 +1,8 @@
 package mo.essam.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
 
 @ManagedBean
@@ -7,6 +10,9 @@ public class Student {
 	
 	private String firstName;
 	private String lastName;
+	private String country;
+	private String job;
+	private List<String> jobs;
 	
 	public String getFirstName() {
 		return firstName;
@@ -22,7 +28,32 @@ public class Student {
 	}
 	public Student() {
 		super();
+		jobs = new ArrayList<>();
+		jobs.add("Devlopment Manger");
+		jobs.add("Developer");
+		jobs.add("Tester");
+		jobs.add("Team Leader");
 	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getJob() {
+		return job;
+	}
+	public void setJob(String job) {
+		this.job = job;
+	}
+	public List<String> getJobs() {
+		return jobs;
+	}
+	public void setJobs(List<String> jobs) {
+		this.jobs = jobs;
+	}
+	
+	
 	
 	
 
