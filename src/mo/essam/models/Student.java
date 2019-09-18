ac5736cfd7b4be96ac4b7d4588eb688d9df1f67c
Mohamed Essam <ana3ssam2017@gmail.com>
2019-09-18 17:39:03 +0200

    Using VALIDATION REGULAR EXPRESSIONS in JSF

--- src/mo/essam/models/Student.java
+++ src/mo/essam/models/Student.java
@@ -7,30 +7,35 @@ import javax.faces.bean.ManagedBean;
 
 @ManagedBean
 public class Student {
-	
+
 	private String firstName;
 	private String lastName;
 	private String country;
-	private String job;	
+	private String job;
 	private String email;
-	private int freePasses;	
+	private int freePasses;
 	private String postalCode;
 	private String favoriteLanguage;
 	private String[] skills;
+	private String phoneNumber;
 	private List<String> jobs;
-	
+
 	public String getFirstName() {
 		return firstName;
 	}
+
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+
 	public String getLastName() {
 		return lastName;
 	}
+
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+
 	public Student() {
 		super();
 		jobs = new ArrayList<>();
@@ -41,53 +46,78 @@ public class Student {
 		firstName = "Karim";
 		lastName = "Sayed";
 	}
+
 	public String getCountry() {
 		return country;
 	}
+
 	public void setCountry(String country) {
 		this.country = country;
 	}
+
 	public String getJob() {
 		return job;
 	}
+
 	public void setJob(String job) {
 		this.job = job;
 	}
+
 	public List<String> getJobs() {
 		return jobs;
 	}
+
 	public void setJobs(List<String> jobs) {
 		this.jobs = jobs;
 	}
+
 	public String getFavoriteLanguage() {
 		return favoriteLanguage;
 	}
+
 	public void setFavoriteLanguage(String favoriteLanguage) {
 		this.favoriteLanguage = favoriteLanguage;
 	}
+
 	public String[] getSkills() {
 		return skills;
 	}
+
 	public void setSkills(String[] skills) {
 		this.skills = skills;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public int getFreePasses() {
 		return freePasses;
 	}
+
 	public void setFreePasses(int freePasses) {
 		this.freePasses = freePasses;
 	}
+
 	public String getPostalCode() {
 		return postalCode;
 	}
+
 	public void setPostalCode(String postalCode) {
 		this.postalCode = postalCode;
 	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
 	
+
 }
